db12150382ac9b7dcbb886a590138464edb18c71
Mey68 <ulysse.meyer@gmail.com>
2015-01-17 23:59:23 +0100

    verif sur les books... un peu bourrin :)

--- src/controllers/AddBook.java
+++ src/controllers/AddBook.java
@@ -52,26 +52,84 @@ public class AddBook extends HttpServlet {
 		String anneePubli = request.getParameter("anneePubli");
 		String resume = request.getParameter("resume");
 		
-
+		//****************************************************************
+		//Gestion des erreurs sur le formulaire d'ajout d'un livre :
+		//****************************************************************
+		String alert_msg = "";
+		int error = 0;
+		//sur le Titre :
+		if(titre==""){
+			alert_msg = alert_msg + "Titre vide !<br>";
+			error = error + 1;
+		}
+		//sur le Auteur :
+		if(auteur==""){
+			alert_msg = alert_msg + "Auteur vide !<br>";
+			error = error + 1;
+		}
+		//sur le editeur :
+		if(editeur==""){
+			alert_msg = alert_msg + "Editeur vide !<br>";
+			error = error + 1;
+		}
+		//sur le isbn :
+		if(isbn==""){
+			alert_msg = alert_msg + "ISBN vide !<br>";
+			error = error + 1;
+		} else {
+			if(!isbn.matches("^\\d{10,13}$")){
+				alert_msg = alert_msg + "L'ISBN '" + isbn + "' n'est pas correct !<br>";
+				error = error + 1;
+			}
+		}
+		//sur le pays
+		if(pays==""){
+			alert_msg = alert_msg + "Age vide !<br>";
+			error = error + 1;
+		}
+		//sur le genre :
+		if(genre==""){
+			alert_msg = alert_msg + "Sexe vide !<br>";
+			error = error + 1;
+		}
+		//sur anneePubli :
+		if(anneePubli==""){
+			alert_msg = alert_msg + "Adresse vide !<br>";
+			error = error + 1;
+		} else {
+			if(!anneePubli.matches("^\\d{1,4}$")){
+				alert_msg = alert_msg + "L'année '" + anneePubli + "' n'est pas correcte !<br>";
+				error = error + 1;
+			}
+		}
 		
 		int ap=Integer.parseInt(anneePubli); 
 		long intisbn=Long.parseLong(isbn); 
 		
 		Book b = new Book(0, titre, auteur, editeur, intisbn, pays, genre, ap, resume);
 		
-		BooksDao.insert(b);
-		
-		request.setAttribute("operation","créé");
-		request.setAttribute("titre",titre);
-		request.setAttribute("auteur",auteur);
-		request.setAttribute("editeur",editeur);
-		request.setAttribute("isbn",isbn);
-		request.setAttribute("pays",pays);
-		request.setAttribute("genre",genre);
-		request.setAttribute("anneePubli",anneePubli);
-		request.setAttribute("resume",resume);
-		
-		request.getRequestDispatcher("WEB-INF/StatusBookModifAdd.jsp").forward(request, response);  
+		if(error!=0){
+			request.setAttribute("alert", alert_msg);
+			b.setId(-1);
+			request.setAttribute("bModif", b);
+			request.getRequestDispatcher("WEB-INF/ModifBook.jsp").forward(request, response); 
+		} else {
+
+			
+			BooksDao.insert(b);
+			
+			request.setAttribute("operation","créé");
+			request.setAttribute("titre",titre);
+			request.setAttribute("auteur",auteur);
+			request.setAttribute("editeur",editeur);
+			request.setAttribute("isbn",isbn);
+			request.setAttribute("pays",pays);
+			request.setAttribute("genre",genre);
+			request.setAttribute("anneePubli",anneePubli);
+			request.setAttribute("resume",resume);
+			
+			request.getRequestDispatcher("WEB-INF/StatusBookModifAdd.jsp").forward(request, response); 
+		}
 	}
 }
 
--- src/controllers/ModifBook.java
+++ src/controllers/ModifBook.java
@@ -52,26 +52,93 @@ public class ModifBook extends HttpServlet {
 		String anneePubli = request.getParameter("anneePubli");
 		String resume = request.getParameter("resume");
 		
+		
+		//****************************************************************
+		//Gestion des erreurs sur le formulaire d'ajout d'un livre :
+		//****************************************************************
+		String alert_msg = "";
+		int error = 0;
+		//sur le Titre :
+		if(titre==""){
+			alert_msg = alert_msg + "Titre vide !<br>";
+			error = error + 1;
+		}
+		//sur le Auteur :
+		if(auteur==""){
+			alert_msg = alert_msg + "Auteur vide !<br>";
+			error = error + 1;
+		}
+		//sur le editeur :
+		if(editeur==""){
+			alert_msg = alert_msg + "Editeur vide !<br>";
+			error = error + 1;
+		}
+		//sur le isbn :
+		if(isbn==""){
+			alert_msg = alert_msg + "ISBN vide !<br>";
+			error = error + 1;
+		} else {
+			if(!isbn.matches("^\\d{10,13}$")){
+				alert_msg = alert_msg + "L'ISBN '" + isbn + "' n'est pas correct !<br>";
+				error = error + 1;
+			}
+		}
+		//sur le pays
+		if(pays==""){
+			alert_msg = alert_msg + "Age vide !<br>";
+			error = error + 1;
+		}
+		//sur le genre :
+		if(genre==""){
+			alert_msg = alert_msg + "Sexe vide !<br>";
+			error = error + 1;
+		}
+		//sur anneePubli :
+		if(anneePubli==""){
+			alert_msg = alert_msg + "Adresse vide !<br>";
+			error = error + 1;
+		} else {
+			if(!anneePubli.matches("^\\d{1,4}$")){
+				alert_msg = alert_msg + "L'année '" + anneePubli + "' n'est pas correcte !<br>";
+				error = error + 1;
+			}
+		}
+		
 		int ap=Integer.parseInt(anneePubli); 
 		long intisbn=Long.parseLong(isbn);
 		
 		Book b = new Book(modif, titre, auteur, editeur, intisbn, pays, genre, ap, resume);
 		
-		Book b2=BooksDao.find(modif);
-		if (b2==null) System.out.println("NULL"); 
-		else { 
-			BooksDao.update(b);
+		if(error!=0){
+			request.setAttribute("alert", alert_msg);
+			request.setAttribute("bModif", b);
+			request.getRequestDispatcher("WEB-INF/ModifBook.jsp").forward(request, response); 
+		} else {
+
 			
-			request.setAttribute("operation","modifié");
-			request.setAttribute("titre",titre);
-			request.setAttribute("auteur",auteur);
-			request.setAttribute("editeur",editeur);
-			request.setAttribute("isbn",isbn);
-			request.setAttribute("pays",pays);
-			request.setAttribute("genre",genre);
-			request.setAttribute("anneePubli",anneePubli);
-			request.setAttribute("resume",resume);
-			request.getRequestDispatcher("WEB-INF/StatusBookModifAdd.jsp").forward(request, response);  
+			Book b2=BooksDao.find(modif);
+			if (b2==null){
+				if(modif==-1){
+					//Il s'agit en réalité d'un addbook on lui renvoie donc :
+					request.getRequestDispatcher("AddBook").forward(request, response); 
+				}else{
+				System.out.println("NULL");
+				}
+			}
+			else { 
+				BooksDao.update(b);
+				
+				request.setAttribute("operation","modifié");
+				request.setAttribute("titre",titre);
+				request.setAttribute("auteur",auteur);
+				request.setAttribute("editeur",editeur);
+				request.setAttribute("isbn",isbn);
+				request.setAttribute("pays",pays);
+				request.setAttribute("genre",genre);
+				request.setAttribute("anneePubli",anneePubli);
+				request.setAttribute("resume",resume);
+				request.getRequestDispatcher("WEB-INF/StatusBookModifAdd.jsp").forward(request, response);  
+			}
 		}
 	}
 
